73bdd0f93ae111b1268d685ca19070ebd4c6d0a3
Burak Özkan <burak.ozkan@path.com.tr>
2021-08-31 09:34:40 +0300

    ready product page

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'product.apps.ProductConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
