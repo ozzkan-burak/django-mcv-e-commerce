4da0fdff86f26519d390c9a20d24ed3d90305570
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-17 17:19:28 +0300

    episode10

--- ecommerce/product/admin.py
+++ ecommerce/product/admin.py
@@ -5,17 +5,19 @@ class ProductImageınline(admin.TabularInline):
   model = Images
   extra = 5
 class CategoryAdmin(admin.ModelAdmin):
-  list_display = ['title', 'status']
+  list_display = ['title', 'status',]
   list_filter = ['status']
 
 class ProductAdmin(admin.ModelAdmin):
-  list_display = ['title' ,'category' ,'price' ,'amount' , 'status']
+  list_display = ['title' ,'category' ,'price' ,'amount','image_tag' ,'status']
   list_filter = ['status', 'category']
   inlines = [ProductImageınline]
+  readonly_fields = ('image_tag',)
 
 
 class ImageAdmin(admin.ModelAdmin):
-  list_display = ['title', 'product', 'image']
+  list_display = ['title', 'product', 'image_tag']
+  readonly_fields = ('image_tag',)
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
