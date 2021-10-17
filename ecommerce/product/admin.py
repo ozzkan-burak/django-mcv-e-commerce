b9f9fd9bca4aae7c66bfc03cdc6d5300f87a8374
Burak Özkan <burak.ozkan@path.com.tr>
2021-10-17 15:40:05 +0300

    create image gallery

--- ecommerce/product/admin.py
+++ ecommerce/product/admin.py
@@ -1,7 +1,9 @@
-from product.models import Category, Product
+from product.models import Category, Product, Images
 from django.contrib import admin
 
-# Register your models here.
+class ProductImageınline(admin.TabularInline):
+  model = Images
+  extra = 5
 class CategoryAdmin(admin.ModelAdmin):
   list_display = ['title', 'status']
   list_filter = ['status']
@@ -9,6 +11,12 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
   list_display = ['title' ,'category' ,'price' ,'amount' , 'status']
   list_filter = ['status', 'category']
+  inlines = [ProductImageınline]
+
+
+class ImageAdmin(admin.ModelAdmin):
+  list_display = ['title', 'product', 'image']
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Images, ImageAdmin)
--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -37,3 +37,10 @@ class Product(models.Model):
   update_at = models.DateTimeField(auto_now=True)
   def __str__(self):
     return self.title;
+
+class Images(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  title = models.CharField(max_length=50, blank=True)
+  image = models.ImageField(blank=True, upload_to='images/')
+  def __str__(self):
+    return self.title;
