41c54ab6e5a6278fb855aed1692d31fa14d37d3f
Burak Özkan <burak.ozkan@path.com.tr>
2021-11-13 14:02:54 +0300

    about, references and contanct pages included

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -4,8 +4,13 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from home import views
+
 urlpatterns = [
     path('', include('home.urls')),
+    path('about/', views.about , name='about'),
+    path('references/', views.references , name='references'),
+    path('contact/', views.contact , name='contact'),
     path('product/', include('product.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
--- ecommerce/home/views.py
+++ ecommerce/home/views.py
@@ -7,5 +7,20 @@ from home.models import Setting
 # Create your views here.
 def index(request):
   setting= Setting.objects.get(pk=1)
-  context = {'setting': setting}
+  context = {'setting': setting, 'page': 'home'}
   return render(request, 'index.html', context)
+
+def about(request):
+  setting= Setting.objects.get(pk=1)
+  context = {'setting': setting}
+  return render(request, 'about.html', context)
+
+def references(request):
+  setting= Setting.objects.get(pk=1)
+  context = {'setting': setting}
+  return render(request, 'references.html', context)
+
+def contact(request):
+  setting= Setting.objects.get(pk=1)
+  context = {'setting': setting}
+  return render(request, 'contact.html', context)
